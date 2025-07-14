e17c50ae632df29fb7deae0efe46a1dbe7729397
MohammadJafar Khajeh <mjkhajehg@gmail.com>
2025-07-15 01:43:34 +0330

    Debug

--- Helpers.php
+++ Helpers.php
@@ -4,7 +4,3 @@ namespace MJ\WPORM;
 function class_basename($class) {
     return basename(str_replace('\\', '/', $class));
 }
-
-function current_time($format) {
-    return date('Y-m-d H:i:s');
-}
