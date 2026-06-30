4dd4108c3022e729b09034985d30ce12a8c454d9
MohammadJafar Khajeh <mjkhajehg@gmail.com>
2026-06-30 20:43:15 +0330

    Enhance JsonCast get method to handle null and empty values, and check for JSON errors

--- Casts/JsonCast.php
+++ Casts/JsonCast.php
@@ -3,7 +3,12 @@ namespace MJ\WPORM\Casts;
 
 class JsonCast implements CastableInterface {
     public function get($value) {
-        return json_decode($value, true);
+        if ($value === null || $value === '') return null;
+        $decoded = json_decode($value, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return null;
+        }
+        return $decoded;
     }
     public function set($value) {
         return json_encode($value);
